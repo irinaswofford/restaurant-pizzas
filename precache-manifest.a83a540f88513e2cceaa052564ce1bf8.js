f6d1928ba89eb2597c4f7509e2cc92194620f440
unknown <irinaswofford@gmai.com>
2020-04-13 18:28:20 -0500

    Updates

---
+++ precache-manifest.a83a540f88513e2cceaa052564ce1bf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b3cc04bdab66f92d36ce8ede35088d5",
+    "url": "/restaurant-pizzas/index.html"
+  },
+  {
+    "revision": "df5fbccbba541faea548",
+    "url": "/restaurant-pizzas/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "6a86701605796a903ab3",
+    "url": "/restaurant-pizzas/static/css/main.285367cc.chunk.css"
+  },
+  {
+    "revision": "df5fbccbba541faea548",
+    "url": "/restaurant-pizzas/static/js/2.1f5bac94.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/restaurant-pizzas/static/js/2.1f5bac94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a86701605796a903ab3",
+    "url": "/restaurant-pizzas/static/js/main.8050f0ec.chunk.js"
+  },
+  {
+    "revision": "b022638827cce93768af",
+    "url": "/restaurant-pizzas/static/js/runtime-main.3aa848c6.js"
+  }
+]);
